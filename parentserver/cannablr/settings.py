1c30ed4d634243b0a642b501a32ed6add895502d
Ben Donnaloia <ben.donnaloia@gmail.com>
2017-02-23 19:28:05 -0800

    changed permissions of static content

--- parentserver/cannablr/settings.py
+++ parentserver/cannablr/settings.py
@@ -5,7 +5,7 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir
 SECRET_KEY = 'ghdfhdf6dhdgddh6'
 
 ALLOWED_HOSTS=['*']
-DEBUG=True
+DEBUG=False
 
 WSGI_APPLICATION = 'cannablr.wsgi.application'
 
@@ -56,26 +56,27 @@ MEDIA_URL = "/media/"
 # Don"t put anything in this directory yourself; store your static files
 # in apps" "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PACKAGE_ROOT, "static1")
+STATIC_ROOT = os.path.join(PACKAGE_ROOT, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = "/static/"
 
 # Additional locations of static files
-STATICFILES_DIRS = [
-    os.path.join(PACKAGE_ROOT, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(PACKAGE_ROOT, "static"),
+# ]
 
 # List of finder classes that know how to find static files in
 # various locations.
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
+# STATICFILES_FINDERS = [
+#     "django.contrib.staticfiles.finders.FileSystemFinder",
+#     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+# ]
 
 
 MIDDLEWARE_CLASSES = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
